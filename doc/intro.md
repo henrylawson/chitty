2cad7ed42e585472ce1c9723dc0eaf7af2a2d4e3
Henry Lawson <hlawson@thoughtworks.com>
2012-11-26 23:11:26 +0530

    Basic chitty leiningen framework

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chitty
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/chitty/core_test.clj
@@ -0,0 +1,7 @@
+(ns chitty.core-test
+  (:use clojure.test
+        chitty.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
