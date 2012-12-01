685d34a41bec62cd3bb86bdfde46f3be6bd3436b
Henry Lawson <hlawson@thoughtworks.com>
2012-12-02 00:16:22 +0530

    Removed all dodgey error handlin

--- src/chitty/main.clj
+++ src/chitty/main.clj
@@ -5,36 +5,29 @@
 	(:use [chitty.messages]))
 
 (defn parse-int [number-string]
-	(try
-		(Integer. (re-find #"[0-9]*" number-string))
-		(catch Exception e nil)))
+	(Integer. number-string))
 
 (defn get-input [prompt]
 	(println prompt)
 	(read-line))
 
 (defn prompt-for-move [player]
-	(let [input (get-input (message-player-move player))]
-		(if (= input "exit")
-			false
-			(parse-int input))))
+	(parse-int (get-input (message-player-move player))))
 
 (defn play-game [board]
 	(loop [	player 0
 			next-player 1
 			move (prompt-for-move player)
 			board board]
-		(if (false? move)
-			(println (messages-goodbye))
-			(let [new-board (place board player move)]
-				(print-board new-board)
-				(cond 
-					(is-game-won? new-board)
-			    	(println (messages-player-won player))
-			    	(is-game-over? new-board)
-			   		(println (messages-game-over))
-			   		:else
-			   		(recur next-player player (prompt-for-move next-player) new-board))))))
+		(let [new-board (place board player move)]
+			(print-board new-board)
+			(cond 
+				(is-game-won? new-board)
+		    	(println (messages-player-won player))
+		    	(is-game-over? new-board)
+		   		(println (messages-game-over))
+		   		:else
+		   		(recur next-player player (prompt-for-move next-player) new-board)))))
 
 (defn -main [& args]
 	(let [board (board)]
